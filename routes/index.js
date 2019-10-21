1480e2ed9298c8f8f885d304e891f9484796d5a4
Vira <vira.romanko99@gmail.com>
2019-10-21 19:47:31 -0400

    class working files

--- routes/index.js
+++ routes/index.js
@@ -12,10 +12,31 @@ router.get('/', (req, res) => {
     sql.query(query, (err, result) => {
         if (err) { throw err; console.log(err); }
 
-        console.log(result); // should see objects wrapped in an array
+        //console.log(result); // should see objects wrapped in an array
 
         // render the home view with dynamic data
         res.render('home', { data: result });
+
+       
+    })
+})
+//looling for localhost:3000/anything
+router.get('/:id', (req, res) => {
+    console.log("hit a dynamic route");
+    console.log(req.params.id);
+
+
+    let query = `SELECT  * FROM tbl_bio WHERE profID="${req.params.id}"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { throw err; console.log(err); }
+
+        console.log(result); // should see objects wrapped in an array
+
+        // render the home view with dynamic data
+        //res.render('home', { data: result });
+
+       
     })
 })
 
