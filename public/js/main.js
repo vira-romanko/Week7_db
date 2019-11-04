c8972bd4dc0d7862fcd36e75f3ae5a01db71e3c3
Vira <vira.romanko99@gmail.com>
2019-11-04 17:22:20 -0500

    more

--- public/js/main.js
+++ public/js/main.js
@@ -39,7 +39,7 @@ const myVM = (() => {
         // find the image closet to the anchor tag and get its src property
 
         let imgSrc = this.previousElementSibling.getAttribute('src');
-        let url = `/${this.getAttribute('href')}`;//  /3 route in express. passing data dynamically
+        let url = `/users/${this.getAttribute('href')}`;//  /3 route in express. passing data dynamically
 
         fetch(url)// go get the data fetch boy!
           .then(res => res.json())// parse the json result into a plain object
--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.get('/', (req, res) => {
     })
 })
 //looling for localhost:3000/anything
-router.get('/:id', (req, res) => {
+router.get('/users/:id', (req, res) => {
     console.log("hit a dynamic route");
     console.log(req.params.id);
 
@@ -37,11 +37,11 @@ router.get('/:id', (req, res) => {
         // turn our social property into an array - its just text in the DB
         // wich isnt really anything we can work with
 
-        //  result[0].social = result[0].social.split(',').map(function(item) {
-        //     item = item.trim(); //remove the extra spaces from each word
+         result[0].social = result[0].social.split(',').map(function(item) {
+         item = item.trim(); //remove the extra spaces from each word
 
-        //     return item;
-        //  });
+         return item;
+          });
        //console.log('after split: ', result[0]);
       
 
